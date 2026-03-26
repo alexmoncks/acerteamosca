e6ab132a530ba301e198587c745f77e365a41fc0
alexmoncks <alex@marramoncks.com.br>
2026-03-26 15:54:35 -0300

    feat: add centralized ads config for AdsTerra integration

---
+++ src/config/ads.js
@@ -0,0 +1,23 @@
+export const adsConfig = {
+  adsterra: {
+    popunder: {
+      enabled: true,
+      src: "https://pl28985691.profitablecpmratenetwork.com/e8/af/5e/e8af5e1ef8758093ed62d268ab3de5e7.js",
+      frequency: "daily",
+    },
+    socialBar: {
+      enabled: true,
+      src: "https://pl28985692.profitablecpmratenetwork.com/12/ca/32/12ca32b5c5c5b71f06f0e474d1df17c8.js",
+    },
+    banner: {
+      enabled: true,
+      key: "2ff84ce2802bd4665950a480fe0d5b07",
+      width: 468,
+      height: 60,
+      invokeUrl: "https://www.highperformanceformat.com/2ff84ce2802bd4665950a480fe0d5b07/invoke.js",
+    },
+  },
+  adsense: {
+    enabled: true,
+  },
+};
